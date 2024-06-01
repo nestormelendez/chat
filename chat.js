a08e2f9bb0da2987e60dae73298089e7de8be75e
Nestor Melendez <melendezinv4321@gmail.com>
2024-06-01 16:58:50 -0400

    message send

--- chat.js
+++ chat.js
@@ -36,25 +36,30 @@ let user = [
   },
 ];
 
-let userActive = {
-  id: "1",
-  userName: "Nestor",
-  email: "Nestor",
-  password: "Nestor0208",
-  photo: "./assets/nestor.jpg",
-};
+let userActive = {};
 
 let pageLogin = document.getElementById("page-login");
 let pageChat = document.getElementById("page-chat");
+let btnNewChat = document.getElementById("btn-new-chat");
+let bubbleContainer = document.getElementById("bubble-container");
+let perfilUser = document.getElementById("perfil-user");
+
+
+
 let btnSignUp = document.getElementById("btn-sign-up");
 let password = document.getElementById("password-input");
 let email = document.getElementById("user-input");
 let chatContactsContainer = document.getElementById("chat-contacts-container");
+const channel = new BroadcastChannel("chat");
+
+
 
 document.addEventListener("click", (e) => {
   if (e.target.matches(".user-out")) {
     pageChat.classList.toggle("disguise");
-    pageLogin.classList.toggle("disguise");
+    // pageLogin.classList.toggle("disguise");
+    // btnNewChat.classList.toggle("disguise");
+
   }
 
   if (e.target.matches(".btn-sign-up")) {
@@ -65,8 +70,46 @@ document.addEventListener("click", (e) => {
       alert("el campo contraseña es obligatorio");
       return false;
     }
-    pageChat.classList.toggle("disguise");
-    pageLogin.classList.toggle("disguise");
+
+    userActive = {};
+
+    verifyEmail(user, email.value);
+
+    if (!userActive.email) {
+      alert("Disculpe el usuario no se encuentra registrado");
+      return;
+    }
+
+    if (userActive.password == password.value) {
+
+      perfilUser.innerHTML = `<header class="perfil-user">
+      <div class="user-header-contact">
+          <img class="image-user-active" src="${userActive.photo}" alt="${userActive.userName}">
+          <span class="name-contact">${userActive.userName}</span>
+      </div>
+      <button class="btn user-out --btn-options">x</button>
+  </header>`
+
+
+
+      for (let index = 0; index < user.length; index++) {
+        const element = user[index];
+        bubbleContainer.innerHTML += `
+        <div id="bubble-${index}" class="bubble-contact">
+                        <img class="bubble-contact" data-bubble="${index}" src="${element.photo}" alt="${element.userName}">
+                    </div>
+        `
+
+      }
+
+
+
+      pageLogin.classList.toggle("disguise");
+      // pageChat.classList.toggle("disguise");
+      // btnNewChat.classList.toggle("disguise");
+
+      
+    }
   }
 
   if (e.target.matches(".--btn-new-chat")) {
@@ -89,16 +132,21 @@ document.addEventListener("click", (e) => {
     if (userStorage) {
       messages = JSON.parse(userStorage);
     }
-
-    messages.push({
+    console.log(chatInput, receiver.userName, position);
+    let newMessage = {
       id: messages.length ? messages[messages.length - 1].id + 1 : 1,
       message: chatInput,
       sender: userActive.userName,
       receiver: receiver.userName,
-    });
+      position: position,
+    }
+    messages.push(newMessage);
+    console.log(messages);
+
+    channel.postMessage(JSON.stringify(newMessage));
 
     const userToSend = JSON.stringify(messages);
-
+    console.log(userToSend);
     localStorage.setItem(userActive.userName, userToSend);
 
     document.getElementById(`input-chat-contact-${position}`).value = "";
@@ -171,6 +219,47 @@ document.addEventListener("click", (e) => {
     elementRemove.parentNode.removeChild(elementRemove);
   }
 });
+
+channel.addEventListener("message", (event) => {
+  const receivedMessage = JSON.parse(event.data);
+  console.log("receivedMessage", receivedMessage)
+  if (receivedMessage) {
+
+    let lugar  = user.findIndex(u => u.userName === receivedMessage.sender);
+    let chatActive = document.getElementById(`chat-${lugar}`);
+    console.log(chatActive, lugar, receivedMessage);
+    
+    chatActive.innerHTML += `<br> <div class="messageReceived"><blockquote>${receivedMessage.receiver}</blockquote> <br> <span>${receivedMessage.message} <span></div>`;
+
+    const userStorage = localStorage.getItem(userActive.userName);
+
+    let messages = [];
+    if (userStorage) {
+      messages = JSON.parse(userStorage);
+    }
+
+    messages.push({
+      id: messages.length ? messages[messages.length - 1].id + 1 : 1,
+      message: receivedMessage.message,
+      sender: receivedMessage.sender,
+      receiver: receivedMessage.receiver,
+      position: lugar,
+    });
+
+    const userToSend = JSON.stringify(messages);
+
+    localStorage.setItem(userActive.userName, userToSend);
+  }
+});
+
+
+function verifyEmail(user, email) {
+  for (const usuario of user) {
+    if (usuario.email === email) {
+      userActive = usuario;
+    }
+  }
+}
 
 // console.log(chatActive)
 // localStorage.setItem(chatInput)
