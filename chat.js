8cf7c3941fc29bd89b92a18aa088c37a5a2a996c
Nestor Melendez <melendezinv4321@gmail.com>
2024-06-01 00:57:04 -0400

    local storage

--- chat.js
+++ chat.js
@@ -36,8 +36,6 @@ let user = [
   },
 ];
 
-let localStorage = window.localStorage;
-
 let userActive = {
   id: "1",
   userName: "Nestor",
@@ -82,10 +80,57 @@ document.addEventListener("click", (e) => {
     let chatInput = document.getElementById(
       `input-chat-contact-${position}`
     ).value;
+    let receiver = user[position];
     chatActive.innerHTML += `\n <div class="messageSend"><blockquote>${userActive.userName}</blockquote> \n <span>${chatInput} <span></div>`;
+
+    const userStorage = localStorage.getItem(userActive.userName);
+
+    let messages = [];
+    if (userStorage) {
+      messages = JSON.parse(userStorage);
+    }
+
+    messages.push({
+      id: messages.length ? messages[messages.length - 1].id + 1 : 1,
+      message: chatInput,
+      sender: userActive.userName,
+      receiver: receiver.userName,
+    });
+
+    const userToSend = JSON.stringify(messages);
+
+    localStorage.setItem(userActive.userName, userToSend);
+
     document.getElementById(`input-chat-contact-${position}`).value = "";
     chatActive.scrollTop = `9999`;
   }
+
+  if (e.target.matches(".bubble-contact")) {
+    let position = e.target.dataset.bubble;
+    let receiver = user[position];
+    let chatContactsContainer = document.getElementById(
+      "chat-contacts-container"
+    );
+    chatContactsContainer.innerHTML += `<div id="chat-content-contact-${position}" class="chat-content-active">
+    <header data-index="${position}" class="content-header-footer">
+        <div class="user-header-contact">
+            <img class="image-contact" src="${receiver.photo}" alt="${receiver.userName}">
+            <span class="name-contact">${receiver.userName}</span>
+        </div>
+        <button data-index="${position}" id="btn-chat-down-contact-${position}"
+            class="btn btn-chat-down-contacts --btn-arrow">↕️</button>
+        <button data-index="${position}" class="btn --btn-delete">x</button>
+    </header>
+    <div id="chat-${position}" class="is-active no-active"></div>
+    <footer data-index="${position}" class="content-header-footer">
+        <input data-index="${position}" id="input-chat-contact-${position}" class="input input-chat-contacts"
+            type="text" placeholder="Chat con ${receiver.userName}">
+        <button data-index="${position}" id="btn-chat-send-contact-${position}"
+            class="btn --btn-chat-send-contacts">✉️</button>
+    </footer>
+</div>`;
+  }
+
   if (e.target.matches(".btn-chat-down-contacts")) {
     let position = e.target.dataset.index;
     let chatInput = document.getElementById(
@@ -94,6 +139,12 @@ document.addEventListener("click", (e) => {
     let chatActive = document.getElementById(`chat-${position}`);
     chatActive.classList.toggle("no-active");
     chatInput = "";
+  }
+
+  if (e.target.matches(".--btn-delete")) {
+    let element = e.target.dataset.index;
+    let elementRemove = document.getElementById(`chat-content-contact-${element}`) 
+    elementRemove.parentNode.removeChild(elementRemove);
   }
 });
 
